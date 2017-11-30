d8f0667e746c02cc81e5c853043d4a0233bc4a21
Richard Chen <siyu.194@gmail.com>
2017-11-30 16:27:44 -0500

    Add files via upload
    add Main.java

---
+++ bin/shadojava-master/src/Engine/Main.java
@@ -0,0 +1,64 @@
+package Engine;
+
+import Input.FileWizard;
+import Input.loadparam;
+import Output.DataWrapper;
+import Output.OutputTest;
+import Output.ProcRep;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+/***************************************************************************
+ *
+ * 	FILE: 			Main.java
+ *
+ * 	AUTHOR: 		ROCKY LI
+ *
+ * 	LATEST_EDIT:	2017/9/12
+ *
+ * 	VER: 			1.2
+ *
+ * 	Purpose: 		Entry point.
+ *
+ **************************************************************************/
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		// LOAD the parameter file.
+
+		String head = FileWizard.getabspath();
+
+		loadparam data;
+
+		if (args.length == 0){
+			data =  new loadparam(head + "/in/params.txt");
+		} else {
+			data = new loadparam(args[0]);
+		}
+//		SCHEN 11/10/17 Test for Reading Fleet Hetero
+//		String strHetero = data.fleetHetero.toString();
+		System.out.println("FleetHetero: "+ Arrays.deepToString(data.fleetHetero));
+		System.out.println("Fleet Types: "+ data.fleetTypes);
+		System.out.println("numTrains: "+ Arrays.toString(data.numTrains));
+
+
+
+		// Runs simulation
+
+		Simulation sim = new Simulation(data);
+		sim.run();
+
+		// Generate Output
+
+		DataWrapper analyze = new DataWrapper(sim, data);
+		analyze.output();
+
+	}
+
+}
